992d7b1131ea001f8cab06552e3b79e3f2c6a3f5
Ruslan Tyan <rtyan@plesk.com>
2022-03-22 03:00:36 +0700

    fixup! Fix according review and move old solution to the another file.

--- src/OldSolution.php
+++ src/OldSolution.php
@@ -1,42 +1,40 @@
 <?php
 declare(strict_types=1);
 
-namespace PhpCourse\Solution;
+namespace PhpCourse\OldSolution;
 
 function binarySum(string $num1, string $num2): string
 {
     // Check only allowed symbols '0' or '1' are passed to variables
     if (!isBynaryString($num1) || !isBynaryString($num2)) {
-        print_r("Error: Variables \$num1: '{$num1}' and \$num2: 
-            '{$num2}' must contains only '0' and '1' symbols.\n");
+        print_r("Error: Variables \$num1: '{$num1}' and \$num2:"
+            . "'{$num2}' must contains only '0' and '1' symbols.\n");
         return 'Error: non binary arguments';
     }
 
     // Check arguments are less than can be safely converted to integer abs($arg) <= PHP_INT_MAX
     // TBD: Rewrite to ifs - see bad tests 1
-    switch (false) {
-        case isMaxSizeExceeded($num1):
-            print_r("Error: Variable \$num1 is greater than max allowed by system\n");
-        case isMaxSizeExceeded($num2):
-            print_r("Error: Variable \$num2 is greater than max allowed by system\n");
-            return 'Error: Arguments are too big';
+    if (isMaxSizeExceeded($num1)) {
+        print_r("Error: Variable \$num1:'{$num1}' is greater than max allowed by system\n");
     }
-
+    if (isMaxSizeExceeded($num2)) {
+        print_r("Error: Variable \$num2:'{$num2}' is greater than max allowed by system\n");
+    }
+    if (isMaxSizeExceeded($num1) || isMaxSizeExceeded($num2)) {
+        return 'Error: Too big arguments';
+    }
     // Convert to integers before summ
     $num1dec = strictBinDec($num1);
     $num2dec = strictBinDec($num2);
 
     // Summ the integers
     $resultDec = $num1dec + $num2dec;
-    if (gettype($resultDec) === 'integer') {
+    if (is_int($resultDec)) {
         return decbin($resultDec);
-    } else {
-        print_r("Summ of the binaries is greater than maximum allowed integer in the system. 
-            Summ:\n{$resultDec}\n");
-        return "Error: Summ is too big number";
     }
-
-
+    print_r("Summ of the arguments is greater than maximum allowed integer in the system."
+        . "Summ:\n{$resultDec}\n");
+    return "Error: Summ is too big number";
 }
 
 function isBynaryString(string $str): bool
@@ -49,23 +47,22 @@ function isMaxSizeExceeded(string $str): bool
     // Maximum integer in binary
     $systemBitsNum = strlen(decbin(PHP_INT_MAX)) + 1;
     if (strlen($str) > $systemBitsNum) {
-        print_r("Argument {$str} has more bits than allowed by
-            system to convert to integers: {$systemBitsNum}\n");
-        return false;
+        print_r("Argument {$str} has more bits than allowed by "
+            . "system to convert to integers: {$systemBitsNum}\n");
+        return true;
     }
-    return true;
+    return false;
 }
 
 function strictBinDec(string $binStr): int
 {
-    if (gettype(bindec($binStr)) === 'integer') {
+    if (is_int(bindec($binStr))) {
         return bindec($binStr);
-    } else {
-        // Convert back from two's compliment numbers (~abs($i)+1)
-        $substr = substr($binStr, 1);
-        $res = -(~bindec($substr)+2 + PHP_INT_MAX);
-        return $res;
     }
+    // Convert back from two's compliment numbers (~abs($i)+1)
+    $substr = substr($binStr, 1);
+    $res = -(~bindec($substr) + 2 + PHP_INT_MAX);
+    return $res;
 }
 
 function myPrint(string $str): void
@@ -79,11 +76,11 @@ function myPrint(string $str): void
 //myPrint(binarySum(decbin(-1),decbin(0))); // -1 + 0 = -1 == | 1111111111111111111111111111111111111111111111111111111111111111
 //myPrint(binarySum(decbin(-1),decbin(+1))); // -1 + 1 = 0
 //myPrint(binarySum(decbin(-PHP_INT_MAX),decbin(+0))); // -PHP_INT_MAX + 0 = -PHP_INT_MAX == |1000000000000000000000000000000000000000000000000000000000000001
+//myPrint(binarySum(decbin(-PHP_INT_MAX),decbin(-1))); // -PHP_INT_MAX - 1  = -PHP_INT_MAX == |1000000000000000000000000000000000000000000000000000000000000000
 //myPrint(binarySum(decbin(PHP_INT_MAX),'0')); // PHP_INT_MAX + 0 = PHP_INT_MAX == |111111111111111111111111111111111111111111111111111111111111111
 //myPrint(binarySum(decbin(PHP_INT_MAX),decbin(-PHP_INT_MAX))); // PHP_INT_MAX + -PHP_INT_MAX = 0
 //myPrint(binarySum(decbin(PHP_INT_MAX),decbin(PHP_INT_MAX))); // Error - summ is too big
 //myPrint(binarySum(decbin(-PHP_INT_MAX),decbin(PHP_INT_MAX) . '11')); // Error - argument is too big
-
-// Bad tests (should be fixed):
-myPrint(binarySum(decbin(-PHP_INT_MAX),decbin(PHP_INT_MAX) . '11')); // Error - argument is too big
-myPrint(binarySum(decbin(-PHP_INT_MAX-33),decbin(PHP_INT_MAX))); // Error - argument is too big
+//myPrint(binarySum(decbin(-PHP_INT_MAX),decbin(PHP_INT_MAX) . '11')); // Error - argument is too big
+//myPrint(binarySum(decbin(-PHP_INT_MAX) . '1',decbin(PHP_INT_MAX))); // Error - argument is too big
+//myPrint(binarySum(decbin(-PHP_INT_MAX+33),decbin(PHP_INT_MAX))); // 100001 // 33
