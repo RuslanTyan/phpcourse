99218cb3ea09a8db96c8875f8693683d879164b4
Ruslan Tyan <rtyan@plesk.com>
2022-03-29 00:55:27 +0700

    Move AddDigits into a separate class

---
+++ src/Tasks/AddDigits.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace PhpCourse\Tasks;
+
+use InvalidArgumentException;
+
+class AddDigits
+{
+    public function addDigits(int $num): int
+    {
+        // Check if argument is >= 0
+        if ($num < 0) {
+            throw new InvalidArgumentException("Error: the argument {$num} is less than zero");
+        }
+        // If argument < 10 - it's one digit already and must be returned
+        if ($num < 10) {
+            return $num;
+        }
+        // If argument is > 10 its digits should be summarized
+        $string = (string)$num; // can be $stringLength = strlen($string = (string) $num);
+        $result = 0;
+        $stringLength = strlen($string);
+        for ($i = 0; $i < $stringLength; $i++) {
+            $result += (int)$string[$i];
+        }
+        return $this->addDigits($result);
+    }
+}
--- src/index.php
+++ src/index.php
@@ -4,8 +4,47 @@ declare(strict_types=1);
 namespace PhpCourse;
 
 require_once 'Tasks/FizzBuzz.php';
+require_once 'Tasks/AddDigits.php';
 
+use AssertionError;
+use InvalidArgumentException;
+use PhpCourse\Tasks\AddDigits;
 use PhpCourse\Tasks\FizzBuzz;
+use Throwable;
+
+function testAddDigits(array $arguments): void
+{
+    $theDigit = new AddDigits();
+    foreach ($arguments as $key => $value) {
+        try {
+            $addDigitsValue = $theDigit->addDigits($key);
+        } catch (InvalidArgumentException $exception) {
+            echo $exception->getMessage(), PHP_EOL;
+            continue;
+        }
+        if ($addDigitsValue === $value) {
+            echo "addDigits({$key}) = {$value}", PHP_EOL;
+            continue;
+        }
+        throw new AssertionError("Incorrect value of addDigits({$key}) = {$addDigitsValue}, expected {$value}" . PHP_EOL);
+    }
+}
+
+// Tests addDigits
+$arguments = [
+    -1 => -1,
+    0 => 0,
+    1 => 1,
+    9 => 9,
+    10 => 1,
+    38 => 2,
+    37 => 2, // incorrect value
+];
+try {
+    testAddDigits($arguments);
+} catch (Throwable $exception) {
+    echo $exception->getMessage();
+}
 
 // FizzBuzz Tests
 $fbz = new FizzBuzz();
